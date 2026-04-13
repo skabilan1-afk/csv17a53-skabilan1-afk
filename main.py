830aa6e17a85763cf3660ab062118dc9421af971
Swathi Kabilan <skabilan@uci.edu>
2026-04-12 18:59:27 -0700

    test find non injective pair doesn't work

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ def find_non_injective_pair(mapping: dict) -> tuple | None:
     """Return (x1, x2) where f(x1)==f(x2) and x1!=x2, or None if injective."""
     # === TODO ===
     # Your code here
-    pass
+    for key1 in mapping:
+        for key2 in mapping:
+            if key1 != k2 and mapping[k1] == mapping[k2]:
+                return (k1, k2)
+    
+    return None
+    
     # === END TODO ===
 
 
@@ -13,7 +19,12 @@ def find_non_surjective_element(mapping: dict, target: set):
     """Return one target element with no input mapping to it, or None if surjective."""
     # === TODO ===
     # Your code here
-    pass
+    
+    for element in target:
+        if element not in mapping.values():
+            return element
+    
+    return None
     # === END TODO ===
 
 
@@ -21,7 +32,7 @@ def my_floor(x: float) -> int:
     """Return floor(x) without using math.floor."""
     # === TODO ===
     # Your code here
-    pass
+    return int(x)
     # === END TODO ===
 
 
@@ -29,7 +40,7 @@ def my_ceil(x: float) -> int:
     """Return ceil(x) without using math.ceil."""
     # === TODO ===
     # Your code here
-    pass
+    return int(x) - 1
     # === END TODO ===
 
 #starter code commit
